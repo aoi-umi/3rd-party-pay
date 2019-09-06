ceae270889e438cb558199efe450c4ed44986f84
pc-xl <1641622365@qq.com>
2019-09-06 12:02:26 +0800

    fix bug

--- src/lib/alipay/base.ts
+++ src/lib/alipay/base.ts
@@ -286,7 +286,7 @@ export class AliPayStatic extends PayStatic {
 
             let rs = await utils.request(reqData);
             let data = rs.data;
-            log.orginRes = data;
+            log.orginRes = { ...data };
             if (opt.originalResult)
                 return data as T;
             data = opt.resDataKey ? data[opt.resDataKey] : data;
